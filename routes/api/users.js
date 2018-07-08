6d19a1a0ae3fb5f2b1986bd68f966a134937c91d
nicholasnbg <nicholasnbg@gmail.com>
2018-07-08 23:13:31 +1000

    Moved keys file to correct path added passport js config file added test route to api/users

---
+++ routes/api/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+
+// Load User model
+const User = require('../../models/User');
+
+router.get('/test', (req, res) => res.json({
+  msg: 'Users works'
+}));
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const passport = require('passport')
+
+const users = require('./routes/api/users');
+
+const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('mongoDB connected'))
+  .catch((err) => console.log(err))
+
+// Passport Middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
+
+// Use routes
+app.use('/api/users', users);
+
+//Set Port
+const port = process.env.port || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+});
