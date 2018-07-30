64cba2d2d13438105091f9a3458df0d31f3e658d
nicholasnbg <nicholasnbg@gmail.com>
2018-07-30 21:23:47 +1000

    Slight changes to group layout

--- client/src/components/group/GroupMembers.js
+++ client/src/components/group/GroupMembers.js
@@ -2,6 +2,13 @@ import React from "react";
 
 const GroupMembers = props => {
   const { members, isAdmin } = props;
+
+  const styles = {
+    li_span: {
+      flexGrow: 1,
+      width: "33%"
+    }
+  };
   return (
     <ul className="list-group">
       {members.map(member => (
@@ -9,12 +16,12 @@ const GroupMembers = props => {
           key={member._id}
           className="list-group-item d-flex justify-content-between"
         >
-          <span className="d-flex">
+          <span className="d-flex" style={styles.li_span}>
             <h6 className="mb-0">{member.user.name}</h6>
             {member.admin ? <small className="ml-3 mt-1">Admin</small> : null}
           </span>
-          <span> {`$${member.balance}`}</span>
-          <span>
+          <span style={styles.li_span}> {`$${member.balance}`}</span>
+          <span className="d-flex justify-content-end" style={styles.li_span}>
             {isAdmin && !member.admin ? (
               <button
                 onClick={() => props.removeMember(member.user._id)}
